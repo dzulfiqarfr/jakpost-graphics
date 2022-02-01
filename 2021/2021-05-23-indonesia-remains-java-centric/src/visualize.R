6450df09dea97689f9b6bae690e953e58632c705
Dzulfiqar Fathur Rahman <frdzulfiqar@gmail.com>
2022-02-01 17:07:08 +0700

    Initial commit with code, data on population distribution

---
+++ 2021/2021-05-23-indonesia-remains-java-centric/src/visualize.R
@@ -0,0 +1,116 @@
+# Packages ----
+
+library(conflicted)
+library(here)
+conflict_prefer('here', 'here')
+library(tidyverse)
+conflict_prefer('filter', 'dplyr')
+library(dfrtheme)
+library(patchwork)
+
+dirYear <- '2021'
+dirProject <- '2021-05-23-indonesia-remains-java-centric'
+
+i_am(paste(dirYear, dirProject, 'src', 'visualize.R', sep = '/'))
+
+
+# Plot ----
+
+## Population distribution ----
+
+popDistribution <- read_csv(
+  here(dirYear, dirProject, 'result', 'population-distribution.csv')
+)
+
+popDistributionDec <- popDistribution %>%
+  mutate(
+    island = fct_reorder(island, population_share),
+    population_share = population_share / 100
+  )
+
+palIsland <- c(
+  'Java' = '#00204DFF',
+  'Sumatra' = '#31446BFF',
+  'Sulawesi' = '#666970FF',
+  'Bali & Nusa Tenggara' = '#958F78FF',
+  'Kalimantan' = '#CBBA69FF',
+  'Maluku & Papua' = '#FFEA46FF'
+)
+
+plotPop <- ggplot(
+  popDistributionDec,
+  aes(x = year, y = population_share, fill = island)
+) +
+  geom_col(position = 'stack', width = 3.5) +
+  scale_x_continuous(labels = c('1970', "'80", "'90", '2000', "'10", "'20")) +
+  scale_y_continuous(
+    labels = seq(0, 100, 25),
+    expand = c(0, 0),
+    position = 'right'
+  ) +
+  scale_fill_manual(values = palIsland) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  labs(
+    subtitle = 'Population',
+    x = NULL,
+    y = NULL
+  ) +
+  dfr_theme() +
+  theme(legend.position = 'top')
+
+
+## GRDP distribution ----
+
+GRDPdistribution <- read_csv(
+  here(dirYear, dirProject, 'result', 'grdp-distribution.csv')
+)
+
+GRDPdistributionDec <- GRDPdistribution %>%
+  mutate(
+    island = fct_reorder(island, grdp_share),
+    grdp_share = grdp_share / 100
+  )
+
+plotGRDP <- ggplot(
+  GRDPdistributionDec,
+  aes(x = year, y = grdp_share, fill = island)
+) +
+  geom_col(position = 'stack', width = 0.6, show.legend = FALSE) +
+  scale_x_continuous(
+    breaks = seq(2010, 2020, 2),
+    labels = c('2010', "'12", "'14", "'16", "'18", "'20")
+  ) +
+  scale_y_continuous(
+    labels = seq(0, 100, 25),
+    expand = c(0, 0),
+    position = 'right'
+  ) +
+  scale_fill_manual(values = palIsland) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  labs(
+    subtitle = 'GDP',
+    x = NULL,
+    y = NULL
+  ) +
+  dfr_theme()
+
+
+## Patchwork ----
+
+plotPop +
+  plotGRDP +
+  plot_annotation(
+    title = 'Population concentration follows development pattern',
+    subtitle = 'Population and GDP distribution, by island (percent)',
+    caption = paste0(
+      'Source: Statistics Indonesia (BPS); *The Jakarta Post* analysis<br>',
+      'Chart: JP/Dzulfiqar Fathur Rahman'
+    ),
+    theme = dfr_theme()
+  )
+
+ggsave(
+  here(dirYear, dirProject, 'result', 'population-gdp-distribution.png'),
+  width = 10,
+  height = 5
+)
